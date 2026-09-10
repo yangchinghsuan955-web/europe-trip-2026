797681a00ed91335f9fe0cd5dc1590d4cb15e2db
yangchinghsuan955-web <yangchinghsuan955@gmail.com>
2026-09-11 00:32:28 +0800

    Fix Day 11 navigation button rendering

--- assets/modules/common-runtime-view.js
+++ assets/modules/common-runtime-view.js
@@ -23,6 +23,7 @@
 
     function isNavigableEvent(event) {
       if (!event || !event.place || isAirportPlace((event.type || "") + " " + (event.title || "") + " " + event.place)) return false;
+      if (event.navigable === true) return true;
       var type = String(event.type || "");
       return /景點|活動/.test(type) || (/住宿/.test(type) && isHotelPlace(event.place));
     }
