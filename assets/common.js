38cb31a5fa956c795e0204c20bcc0b8f05d1a6a2
yangchinghsuan955-web <yangchinghsuan955@gmail.com>
2026-09-12 01:24:25 +0800

    Load Day 15 itinerary overrides

--- assets/common.js
+++ assets/common.js
@@ -36,6 +36,7 @@
       document.write('<script src="' + base + 'modules/daily-day11-overrides.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-day12-overrides.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-day13-overrides.js' + '"><\/script>');
+      document.write('<script src="' + base + 'modules/daily-day15-overrides.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-logic.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-scroll.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-render.js' + '"><\/script>');
@@ -65,6 +66,7 @@
       base + "modules/daily-day11-overrides.js",
       base + "modules/daily-day12-overrides.js",
       base + "modules/daily-day13-overrides.js",
+      base + "modules/daily-day15-overrides.js",
       base + "modules/daily-logic.js",
       base + "modules/daily-scroll.js",
       base + "modules/daily-render.js",
