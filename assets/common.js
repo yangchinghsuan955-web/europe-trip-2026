1de49349071006d80ff39a4bda2022962cd0a03c
yangchinghsuan955-web <yangchinghsuan955@gmail.com>
2026-09-15 00:13:41 +0800

    feat(daily): load Day 1 and Day 2 overrides

--- assets/common.js
+++ assets/common.js
@@ -31,6 +31,8 @@
 
     if (isDailyPage) {
       document.write('<script src="' + base + 'data/itinerary.js' + '"><\/script>');
+      document.write('<script src="' + base + 'modules/daily-day1-overrides.js' + '"><\/script>');
+      document.write('<script src="' + base + 'modules/daily-day2-overrides.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-day5-overrides.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-day9-overrides.js' + '"><\/script>');
       document.write('<script src="' + base + 'modules/daily-day10-overrides.js' + '"><\/script>');
@@ -64,6 +66,8 @@
   if (isDailyPage) {
     scripts.push(
       base + "data/itinerary.js",
+      base + "modules/daily-day1-overrides.js",
+      base + "modules/daily-day2-overrides.js",
       base + "modules/daily-day5-overrides.js",
       base + "modules/daily-day9-overrides.js",
       base + "modules/daily-day10-overrides.js",
